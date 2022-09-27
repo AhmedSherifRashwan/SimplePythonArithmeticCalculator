54d9ceffc792ab514b4fad2a4ab1b7d2e5bcba87
rashwana2 <rashwana2@uni.coventry.ac.uk>
2022-09-27 16:24:41 +0100

    My Practice Commit For This Project

---
+++ MathCalc.py
@@ -0,0 +1,6 @@
+num1 = int(input('Input your first number: '))
+num2 = int(input('Input your first number: '))
+print('+: ', num1 + num2)
+print('-: ', num1 - num2)
+print('*: ', num1 * num2)
+print('/: ', num1 / num2)
